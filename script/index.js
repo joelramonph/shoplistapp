265d1453cfd8b7a05e81329bad062ab99ea95d5c
Joel Parra <joelramonph@gmail.com>
2023-04-15 22:45:55 -0400

    add styles main

---
+++ script/index.js
@@ -0,0 +1,9 @@
+
+const inputField = document.getElementById("input-field")
+const addButton = document.getElementById("add-button")
+
+
+addButton.addEventListener("click", function(){
+    let inputValue = inputField.value
+    console.log(inputValue);
+})
