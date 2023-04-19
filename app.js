8e368310db0a135969fb6eee83d658e3fdb38f92
Joel Parra <joelramonph@gmail.com>
2023-04-19 12:09:44 -0400

    add function and styles

---
+++ app.js
@@ -0,0 +1,18 @@
+// let jivitedUsuarios = {
+//     "00": "ivonesantiago@gmail.com",
+//     "01": "andreinaquintero@gmail.com",
+//     "02": "stephanioparra@gmail.com",
+//     "03": "miguelpiza@gmail.com"
+// }
+
+// // console.log(Object.entries(jivitedUsuarios));
+
+// let jivitedEmailUsuarios = Object.values(jivitedUsuarios)
+// console.log(jivitedEmailUsuarios)
+
+// let jivitedUserId = Object.keys(jivitedUsuarios)
+// console.log(jivitedUserId);
+
+// let jivitedUsersEntries = Object.entries(jivitedUsuarios)
+
+// console.log(jivitedUsersEntries);
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
  
 
 const appSetting = {
@@ -8,18 +8,82 @@ const appSetting = {
 
 const app = initializeApp(appSetting)
 const database = getDatabase(app)
-const moviesInDb = ref(database, "movies")
+const shoppingListInDb = ref(database, "shoppingList")
 
 console.log(app);
 
 const inputField = document.getElementById("input-field")
 const addButton = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 
 addButton.addEventListener("click", function(){
     let inputValue = inputField.value
 
-    push(moviesInDb, inputValue)
+    push(shoppingListInDb, inputValue)
 
-    console.log(`${inputValue} added to  database `);
+    clearInputField() // inputField.value = ""
+
+    
+    // appendItemToshoppingListEl(inputValue)
+    
+    // shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+     console.log(`${inputValue} added to  database `);
 })
+
+// Chalenge
+//Call onValue function with shoppingListInDb
+// as firts argument and function(snapshot) as 
+// second argument
+
+onValue(shoppingListInDb, function(snapshot){
+    // reto1: console log snapshot.val para mostrar todos los items
+    // en la database shoppingList
+    // Respuesta console.log(snapshot.val()); 
+
+    //reto2 use el metodo Object.values() para convertir
+    //snapshot.val en un arreglo crea una variable para
+    //almacenar estos
+    
+    let itemsArr = Object.values(snapshot.val())
+
+    clearShoppingListEl()
+    // console.log(itemsArr)
+    
+    //Reto3 Escriba un fro loop para iterar sobre itemsArr
+    // e imprima en console cada item
+    for(let i = 0; i < itemsArr.length; i++){
+        /**
+         Reto4 Use la funcion appendItemToshoppingListEl(inputValue)
+         dentro del loop para abjuntar los elementos 
+         de la shopping list
+         
+         */
+        appendItemToshoppingListEl(itemsArr[i])
+        console.log(itemsArr[i])
+    }
+    
+    
+    
+    
+    
+})
+
+
+//Limpia los elementos del elemento <il></li>
+
+function clearShoppingListEl(){
+    shoppingListEl.innerHTML = ""
+
+}
+
+//Limpia los valores en el elemento input
+function clearInputField(){
+    inputField.value = ""
+}
+
+//Inserta los valores en elemento <li></li>
+function appendItemToshoppingListEl(itemValue){
+
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
